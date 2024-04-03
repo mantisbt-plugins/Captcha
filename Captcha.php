0a9a20e7830d866be414d1ef8ed31862113b91f2
Cas <cas@nuy.info>
2024-04-03 09:04:05 +0200

    Update Captcha.php
    Added focus on input field

--- Captcha.php
+++ Captcha.php
@@ -11,7 +11,7 @@ class CaptchaPlugin extends MantisPlugin  {
 	function register() {
 		$this->name = plugin_lang_get( 'title' );
 		$this->description = plugin_lang_get( 'desc' );
-		$this->version = '1.1.0';
+		$this->version = '1.1.1';
 		$this->requires = array('MantisCore' => '2.3.0-dev',);
 		$this->page      = 'config';
 		$this->author = 'Cas Nuy';
